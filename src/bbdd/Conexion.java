ba92b36d259e19964d30d87ea8026d44935777dd
Jesús <jesusruiver@jesusruiver.com>
2017-11-21 10:47:34 +0100

    Metodo realiza consulta en la clase conexion

--- src/bbdd/Conexion.java
+++ src/bbdd/Conexion.java
@@ -18,7 +18,7 @@ public class Conexion {
 	private String usuario = "";
 
 	private String pass = "";
-	
+
 	private String url = "";
 
 	public Conexion(String database, String usuario, String pass) {
@@ -28,17 +28,16 @@ public class Conexion {
 			this.database = database;
 			this.usuario = usuario;
 			this.pass = pass;
-			
 
 			// Cargar el driver
 			Class.forName("com.mysql.jdbc.Driver");
 
 			url = "jdbc:mysql://localhost/" + database;
-			
+
 			// Establecemos la conexion con la BD
 			conexion = DriverManager.getConnection(url, usuario, pass);
 
-			System.out.println("Conexion a Base de Datos " + url + " . . . . .Ok");
+			System.out.println("Conexion a Base de Datos " + url + " . . . . .Ok\n");
 
 		}
 
@@ -54,6 +53,28 @@ public class Conexion {
 
 		}
 
+	}
+
+	public void realizaConsulta() {
+
+		try {
+
+			// Preparamos la consulta
+			Statement sentencia = conexion.createStatement();
+			String sql = "SELECT * FROM departamentos";
+			ResultSet resul = sentencia.executeQuery(sql);
+
+			// Recorremos el resultado para visualizar cada fila
+			// Se hace un bucle mientras haya registros y se van visualizando
+			while (resul.next()) {
+				System.out.printf("%d, %s, %s %n", resul.getInt(1), resul.getString(2), resul.getString(3));
+
+			}
+			resul.close(); // Cerrar ResultSet
+			sentencia.close(); // Cerrar Statement
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Connection getConexion() {
--- src/logica/Departamento.java
+++ src/logica/Departamento.java
@@ -1,5 +1,19 @@
 package logica;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import bbdd.Conexion;
+
 public class Departamento {
+
+	public static void main(String[] args) {
+
+		Conexion conexion = new Conexion("ejemplo", "root", "root");
+
+		conexion.realizaConsulta();
+		conexion.cerrarConexion();
+	}// fin de main
 
 }
